311390006f1b2dd82c95b2f6ac9704285b07460e
oscarg11 <oscar.g2511@gmail.com>
2024-05-28 11:32:59 -0400

    First solana ssion

---
+++ load-keypair.ts
@@ -0,0 +1,6 @@
+import 'dotenv/config';
+import { getKeypairFromEnvironment } from '@solana-developers/helpers';
+
+const keypair = getKeypairFromEnvironment("SECRET_KEY");
+
+console.log(`Finished! we've loaded our keypair securely, using an env file! Our public key is ${keypair.publicKey.toBase58()}`);
